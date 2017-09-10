3fa306ac8ff329d18762e21aaa3be55a98e64b2d
Disassembler <disassembler@dasm.cz>
2017-09-10 23:35:06 +0200

    Pass command line args into elevation call instead of function args, fixes #26

--- Win10.ps1
+++ Win10.ps1
@@ -1686,9 +1686,10 @@ Function EnableIEEnhancedSecurity {
 ##########
 
 # Relaunch the script with administrator privileges
+$PSCommandArgs = $args
 Function RequireAdmin {
 	If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
-		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $args" -WorkingDirectory $pwd -Verb RunAs
+		Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -WorkingDirectory $pwd -Verb RunAs
 		Exit
 	}
 }
