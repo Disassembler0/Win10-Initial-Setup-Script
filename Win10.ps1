8dfe56cf2eb0812fd25e1cc58723840ef38d608c
Disassembler <disassembler@dasm.cz>
2018-08-23 15:59:37 +0200

    Update "ShowTrayIcons" to use GPO, closes #114 (thx @driscollw)

--- Win10.ps1
+++ Win10.ps1
@@ -1362,13 +1362,16 @@ Function ShowTaskbarPeopleIcon {
 # Show all tray icons
 Function ShowTrayIcons {
 	Write-Output "Showing all tray icons..."
-	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" -Name "EnableAutoTray" -Type DWord -Value 0
+	If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer")) {
+		New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" | Out-Null
+	}
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoAutoTrayNotify" -Type DWord -Value 1
 }
 
 # Hide tray icons as needed
 Function HideTrayIcons {
 	Write-Output "Hiding tray icons..."
-	Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" -Name "EnableAutoTray" -ErrorAction SilentlyContinue
+	Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoAutoTrayNotify" -ErrorAction SilentlyContinue
 }
 
 # Disable search for app in store for unknown extensions
