3b8fc957c1c1fc84b3b0f792321228f8263d1814
Disassembler <disassembler@dasm.cz>
2018-08-27 14:44:52 +0200

    v2.21, 2018-08-27  - Improve UninstallOneDrive in multi-user environment (thx @chiva)  - Improve SetTaskbarCombine* on multi-monitor setups (thx @kroimon)  - Improve DisableTelemetry to not block certain updates (thx @T4CC)  - *BREAKING* Split DisableThumbsDB into DisableThumbnailCache and DisableThumbsDBOnNetwork (thx @r3incarnat0r)  - Add DisableWebLangList (thx @agsimmons)  - Add "Show/Hide item selection checkboxes" (thx @Tikus)  - Add "Uninstall Windows Fax and Scan Services" (thx @kroimon)  - Add sound scheme related tweaks (thx @r3incarnat0r)  - Add "HideQuickAccess" (thx @driscollw)  - Add "Disable NCSI active test" (thx @antipatico)  - Add "Hide Account Protection warning"  - Add "Disable Edge desktop shortcut creation"  - Add more keys to DisableAppSuggestion  - Add more checks for DisableWiFiSense  - Update "ShowTrayIcons" to use GPO (thx @driscollw)  - Update SetP2PUpdate* tweaks to support current builds  - Remove more and more bloatware  - Fix typo in Enable/DisableHibernation  - Update and clarify bunch of comments

--- Win10.ps1
+++ Win10.ps1
@@ -1,7 +1,7 @@
 ##########
 # Win10 / WinServer2016 Initial Setup Script
 # Author: Disassembler <disassembler@dasm.cz>
-# Version: development
+# Version: v2.21, 2018-08-27
 # Source: https://github.com/Disassembler0/Win10-Initial-Setup-Script
 ##########
 
