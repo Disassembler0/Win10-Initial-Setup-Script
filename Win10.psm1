5b2333ba35731d5db16755833cd6ffd17e873ec6
Disassembler <disassembler@dasm.cz>
2018-10-23 22:19:08 +0200

    Add "Hide shortcut icon arrow"

--- Win10.psm1
+++ Win10.psm1
@@ -1550,6 +1550,21 @@ Function EnableShortcutInName {
 	Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer" -Name "link" -ErrorAction SilentlyContinue
 }
 
+# Hide shortcut icon arrow
+Function HideShortcutArrow {
+	Write-Output "Hiding shortcut icon arrow..."
+	If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons")) {
+		New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" -Name "29" -Type String -Value "%windir%\System32\shell32.dll,-50"
+}
+
+# Show shortcut icon arrow
+Function ShowShortcutArrow {
+	Write-Output "Showing shortcut icon arrow..."
+	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons" -Name "29" -ErrorAction SilentlyContinue
+}
+
 # Adjusts visual effects for performance - Disables animations, transparency etc. but leaves font smoothing and miniatures enabled
 Function SetVisualFXPerformance {
 	Write-Output "Adjusting visual effects for performance..."
