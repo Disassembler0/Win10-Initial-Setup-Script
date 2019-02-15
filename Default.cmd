c86e9544d8e3916861541f20c07306b3fd4c126b
Zweikeks <jpc_husky@yahoo.de>
2019-02-15 15:50:17 +0100

    Added call variant with output redirection in .cmd

--- Default.cmd
+++ Default.cmd
@@ -1,1 +1,7 @@
-@powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -preset "%~dpn0.preset"
+@echo off
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0Win10.ps1" -include "%~dp0Win10.psm1" -preset "%~dpn0.preset"
+
+rem The following variant redirects output also to a file
+rem Usage: Default.cmd foo.log
+rem Works only if already started as admin (RequireAdmin in .preset can't be used)
+rem powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "&"'%~dp0Win10.ps1' -include '%~dp0Win10.psm1' -preset '%~dpn0.preset' "2>&1 | Tee-Object" -FilePath '%1'
